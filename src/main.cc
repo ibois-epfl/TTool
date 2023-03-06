3c0dec4fb6dc9fcbb6b8b7a36238d2fe29bdf1a8
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-03-06 18:46:46 +0100

    feat: add live camera

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include "TToolSegment.h"
+#include "camera.hh"
 
 #include <iostream>
 
@@ -7,8 +8,11 @@ int main(int argc, char** argv)
     std::cout << "TEST" << std::endl;
 
     auto seg = ttoolsegment::Segmentation("");
-
-
-
-
+    std::shared_ptr<Camera> camera_ptr(Camera::BuildCamera(""));  // --> "" to open live stream / original: Camera::BuildCamera(gp->frames)
+    while (true) {
+        camera_ptr->UpdateCamera();
+        cv::imshow("From Camera", camera_ptr->image());
+        if (cv::waitKey(1)==27) break;
+    }
+    return 0;
 }
