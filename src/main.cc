fbff7a4e36237270fc7349af26be123328b43d79
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-03-07 14:21:10 +0100

    feat: TSegment integration

--- src/main.cc
+++ src/main.cc
@@ -12,7 +12,14 @@ int main(int argc, char** argv)
     while (true) {
         camera_ptr->UpdateCamera();
         cv::imshow("From Camera", camera_ptr->image());
+        seg.consume_image(camera_ptr->image());
         if (cv::waitKey(1)==27) break;
     }
+    auto mask_pair = seg.get_mask();
+    if (mask_pair.first) {
+        cv::imshow("Mask", mask_pair.second);
+        cv::waitKey(0);
+    }
+
     return 0;
 }
