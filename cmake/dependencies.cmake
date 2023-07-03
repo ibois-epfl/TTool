64fb135a2e1824f94df98224f5a0374cceb54831
9and3 <andreasettimi39@gmail.com>
2023-07-03 13:55:34 +0200

    FIX: modified glmDeps to avoid AC conflicts

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -42,5 +42,5 @@ if (OPT_BUILD_TTOOL_EXE)
     find_package(Qt5OpenGL REQUIRED)
 endif()
 
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/glm)
-set(GLM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/glm)
+# add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
+set(GLM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
