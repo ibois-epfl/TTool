6af940d7e6477fba11ec0a215a765e5b95a502a8
Nicolas Richart <networms@gmail.ch>
2023-07-18 00:33:24 +0200

    [wip] Changing the cmake part to integrate differently in AC

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,19 +1,17 @@
 find_package(OpenCV 4.5.4 REQUIRED)
-if(NOT OpenCV_FOUND)
-    message(FATAL_ERROR "OpenCV 4.5.4 not found")
-endif()
-
 find_package(Threads REQUIRED)
-if(NOT Threads_FOUND)
-    message(FATAL_ERROR "Threads not found")
-endif()
-
-#GLFW
 find_package(glfw3 3.3 REQUIRED)
 find_package(GLEW REQUIRED)
-if(NOT GLEW_FOUND)
-    message(FATAL_ERROR "GLEW not found")
+
+if (TTOOL_BUILD_TTOOL_EXE)
+    find_package(assimp REQUIRED)
+    find_package(glog REQUIRED)
+    find_package(OpenGL REQUIRED)
 endif()
+
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
+list(APPEND TTOOL_INSTALL_TARGETS glm)
+#set(GLM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
 
 # libtorch
 if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
@@ -28,21 +26,3 @@ endif()
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
 # add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch)
 find_package(Torch REQUIRED)
-
-find_package(assimp REQUIRED)
-if(NOT assimp_FOUND)
-    message(FATAL_ERROR "assimp not found")
-endif()
-
-find_package(glog REQUIRED)
-if(NOT glog_FOUND)
-    message(FATAL_ERROR "glog not found")
-endif()
-
-find_package(OpenGL REQUIRED)
-if(NOT OpenGL_FOUND)
-    message(FATAL_ERROR "OpenGL not found")
-endif()
-
-# add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
-set(GLM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
