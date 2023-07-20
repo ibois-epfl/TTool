259acf90817c3ca38d77f41ca86fe58ac4bd663a
aymanns <florian.aymanns@epfl.ch>
2023-07-20 09:43:10 +0200

    Extract frames from test dataset
    Modified the script to extract frames from the videos in the test data set such that they can be used with the existing `ToolDataset` class.

--- ai/videos_to_frames.sh
+++ ai/videos_to_frames.sh
@@ -1,15 +1,21 @@
-DATADIR="/data/ENAC/iBOIS/undistorted_videos"
-IMAGEDIR="/data/ENAC/iBOIS/images"
+DATADIR="/data/ENAC/iBOIS/test_dataset"
+IMAGEDIR="/data/ENAC/iBOIS/test_dataset/images"
 
 for TOOLDIR in $DATADIR/*
 do
     TOOLNAME="$(basename ${TOOLDIR})"
     echo ${TOOLNAME}
+    if [[ "$TOOLNAME" == "images" ]]
+    then
+        echo "in if"
+        continue
+    fi
     for VIDFILE in $TOOLDIR/*
     do
         echo "\t${VIDFILE}"
         VIDNAME="$(basename ${VIDFILE})"
-        # echo "\t\t${VIDNAME%.*}"
+        VIDNAME="$(echo $VIDNAME | sed s/.*-//)"
+        echo "\t\t${VIDNAME%.*}"
         ffmpeg -i $VIDFILE "${IMAGEDIR}/${TOOLNAME}__${VIDNAME%.*}_%09d.png"
     done
 done
