c350a523dfb0baee2e19ec07050462e5f335a314
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-07-21 11:32:17 +0200

    WIP: try extract from fresh download

--- util/extract_libtorch.sh
+++ util/extract_libtorch.sh
@@ -5,3 +5,4 @@ if [ ! -d "$1/assets/libtorch-cxx11-abi-shared-with-deps-2.0.1+cpu.zip" ]; then
 fi
 
 unzip -o $1/assets/libtorch-cxx11-abi-shared-with-deps-2.0.1+cpu.zip -d $1/deps/
+unzip -o $1/assets/libtorch-cxx11-abi-shared-with-deps-2.0.1+cpu.zip.1 -d $1/deps/
