13d57cf698b3e155a043dcf0130f5cfa42b6fad8
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-07-21 11:21:49 +0200

    FIX: libtorch lfs may not exist in docker

--- util/extract_libtorch.sh
+++ util/extract_libtorch.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ ! -d "$1/assets/libtorch-cxx11-abi-shared-with-deps-2.0.1+cpu.zip" ]; then
+  print_process "Downloading libtorch 2.0.1+cpu.zip.."
+  wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcpu.zip -P $1/assets/
+fi
 
 unzip -o $1/assets/libtorch-cxx11-abi-shared-with-deps-2.0.1+cpu.zip -d $1/deps/
