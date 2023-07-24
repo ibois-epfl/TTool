74dd48123ac6e1741947a388d6e8f2e2ae4fa6bf
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-07-24 09:57:23 +0200

    WIP: remove CGAL

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,24 +8,11 @@ if(NOT Threads_FOUND)
     message(FATAL_ERROR "Threads not found")
 endif()
 
+#GLFW
 find_package(glfw3 3.3 REQUIRED)
 find_package(GLEW REQUIRED)
 if(NOT GLEW_FOUND)
     message(FATAL_ERROR "GLEW not found")
-endif()
-
-if (OPT_WITH_CGAL)
-    # use local eigen
-    find_package(Eigen3 3.3 REQUIRED NO_MODULE)
-
-    find_library(GMP_LIB NAMES "gmp" PATHS ${CMAKE_LIBRARY_PATH})
-    include(
-        Eigen3::Eigen
-        gmp
-    )
-    include_directories(
-        ${CMAKE_CURRENT_SOURCE_DIR}/deps/CGAL/include
-    )
 endif()
 
 # libtorch
@@ -37,24 +24,24 @@ if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
     )
 endif()
 
-set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
+# set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
+list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
+# add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch)
 find_package(Torch REQUIRED)
 
-if (OPT_BUILD_TTOOL_EXE)
-    find_package(assimp REQUIRED)
-    if(NOT assimp_FOUND)
-        message(FATAL_ERROR "assimp not found")
-    endif()
+find_package(assimp REQUIRED)
+if(NOT assimp_FOUND)
+    message(FATAL_ERROR "assimp not found")
+endif()
 
-    find_package(glog REQUIRED)
-    if(NOT glog_FOUND)
-        message(FATAL_ERROR "glog not found")
-    endif()
+find_package(glog REQUIRED)
+if(NOT glog_FOUND)
+    message(FATAL_ERROR "glog not found")
+endif()
 
-    find_package(OpenGL REQUIRED)
-    if(NOT OpenGL_FOUND)
-        message(FATAL_ERROR "OpenGL not found")
-    endif()
+find_package(OpenGL REQUIRED)
+if(NOT OpenGL_FOUND)
+    message(FATAL_ERROR "OpenGL not found")
 endif()
 
 # add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/glmT)
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,5 +1,4 @@
-option(OPT_BUILD_4_API      "Set to ON to build TTool just with API"                                            OFF)
-option(OPT_WITH_CGAL        "[DEBUG] Set to ON when start developing Global Rotation"                           OFF)
-option(OPT_BUILD_TTOOL_EXE  "Set to OFF to build TTool without executable"                                      ON)
+option(OPT_BUILD_4_API      "Set to ON to build TTool just with API"                                            ON)
+option(OPT_BUILD_TTOOL_EXE  "Set to OFF to build TTool without executable"                                      OFF)
 option(OPT_REFRESH_DATASET  "Set to ON to refresh dataset, erase the current and download it again"             OFF)
 
