a70aad09eddef7ef27cb3e58d3466c42597a2737
Nicolas Richart <networms@gmail.ch>
2023-07-27 11:43:39 +0200

    Using the lfs file

--- deps/Torch.cmake
+++ deps/Torch.cmake
@@ -1,8 +1,12 @@
 # libtorch
+# download_external_project(libtorch
+#     URL "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcpu.zip"
+#     PATCH libtorch-optional-c++17.patch
+#     THIRD_PARTY_DIR deps)
+
 download_external_project(libtorch
-    URL "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcpu.zip"
-    PATCH libtorch-optional-c++17.patch
-    THIRD_PARTY_DIR deps)
+    URL "file://${PROJECT_SOURCE_DIR}/assets/libtorch-cxx11-abi-shared-with-deps-2.0.1+cpu.zip"
+)
 
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/deps/libtorch")
 find_package(Torch REQUIRED)
