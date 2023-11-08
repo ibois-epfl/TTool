62f6ee77d1ba455036395f3a10fe683838962c26
sushidelivery <nazgul160796@icloud.com>
2023-11-08 18:04:38 +0100

    WIP-ADD: changed the parsing of the data and writing to csv

--- eval/script/acc_estim_pose/io_stream.py
+++ eval/script/acc_estim_pose/io_stream.py
@@ -3,55 +3,71 @@ import csv
 import os
 
 def parse_log_data(data_path):
-    # Open and read the log file
     with open(data_path, 'r') as log_file:
         raw_data = log_file.read()
-    entries = raw_data.strip().split('\n\n')  # Using strip() to handle trailing newlines
+    entries = raw_data.strip().strip('\n').split('\n\n')
     parsed_data = []
 
     for entry in entries:
         lines = entry.split('\n')
         timestamp = lines[0]
+
         name = lines[1].split(',')[1]
-        points = {parts[2]: np.array([float(parts[3]), float(parts[4]), float(parts[5])])
-                  for parts in (line.split(',') for line in lines[1:]) if len(parts) > 2}
-
-        parsed_data.append({"timestamp": timestamp, "name": name, "points": points})
-
+        points = []
+        for line in lines[1:]:
+            coords = line.split(',')
+            if len(coords) > 2:
+                values = float(coords[3]), float(coords[4]), float(coords[5])
+                points.append(values)
+        #{toolhead: [[toolbase, tooltip, holebase holeend],timestamp]}
+        parsed_data.append({name: [points, timestamp]})
     return parsed_data
 
 
-def dump_accuracy_to_csv(results, filename='results.csv'):
+def dump_results_to_csv(results, filename='results.csv'):
+
     csv_filename = os.path.join(filename, "results.csv")
-    headers = ["Name", "Position Difference", "Rotation Difference"]
+    headers = ["Name", "Position_Error", "Position_Diff_Vector" "Rotation_Error"]
 
     with open(csv_filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-
-        # Write the headers first
         csvwriter.writerow(headers)
-
-        # Write data
         for event in results:
             row = [
-                event["name"],
-                event["position_difference"],
-                event["rotation_difference"],
+                list(event)[0],
+                list(event.values())[0][0],
+                list(event.values())[0][1],
+                list(event.values())[0][2]
             ]
             csvwriter.writerow(row)
 
 
-def dump_stats_to_csv(stats_results, path, filename, entry_name):
-    csv_filename = os.path.join(path, filename)
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
+def flatten_stats(name, stat_type, stats):
+    return [name + '_' + stat_type] + [stats[key] for key in ['max', 'min', 'mean', 'median', 'std', 'q1', 'q4']]
 
-        header = ["Name", "Max", "Min", "Mean", "Median", "Std", "First Quartile", "Fourth Quartile"]
-        csvwriter.writerow(header)
 
-        for entry in stats_results:
-            name = entry['name']
-            _entry = entry[entry_name]
-            row = [name, _entry['max'], _entry['min'], _entry['mean'], _entry['median'],
-                   _entry['std'], _entry['first_quartile'], _entry['fourth_quartile']]
-            csvwriter.writerow(row)
+def dump_stats_to_csv(stats_results, path):
+    o_files = ['p_error.csv', 'diff_pos_X.csv', 'diff_pos_Y.csv', 'diff_pos_Z.csv', 'r_error.csv']
+    header = ["Name", "Max", "Min", "Mean", "Median", "Std", "Q1", "Q4"]
+    for o in o_files:
+        csv_f = os.path.join(path, o)
+        with open(csv_f, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+
+            # Write the header row
+            writer.writeheader()
+
+            # Write the data
+            for tool_name, tool_data in stats_results.items():
+                for stat_type, stats in tool_data.items():
+                    row_data = {
+                        'Name': tool_name,
+                        'Max': stats['max'],
+                        'Min': stats['min'],
+                        'Mean': stats['mean'],
+                        'Median': stats['median'],
+                        'Std': stats['std'],
+                        'Q1': stats['q1'],
+                        'Q4': stats['q4']
+                    }
+                    writer.writerow(row_data)
