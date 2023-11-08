103575c62d4c04a3b933ccb06e4bd31495c74706
sushidelivery <nazgul160796@icloud.com>
2023-11-08 18:05:54 +0100

    WIP: added comments on what to do with visuals

--- eval/script/acc_estim_pose/visuals.py
+++ eval/script/acc_estim_pose/visuals.py
@@ -1,13 +1,8 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from tabulate import tabulate
-
-CYBERGREEN = '#2DDE98'
-CYBERGRAPE = '#6F2DBD'
-CYBERBLUE = '#2D9CDB'
-CYBERORANGE = '#F2994A'
+from util import create_out_directory
 
 def plot_rotation_accuracy(csv_file, out_path):
     save_plot = os.path.join(out_path, "rotation_accuracy.png")
@@ -25,9 +20,14 @@ def draw_latex_table(csv_file):
     data = pd.read_csv(csv_file)
     data_dict = data.to_dict(orient='records')
     latex_table = tabulate(data_dict, headers="keys", tablefmt="simple")
+    #TODO export that file
+    print(latex_table)
+
 
 def draw_boxplot_from_csv(csv_file, _name):
+    # TODO: add the values to the plot, do the green color of the mean, y label
     # Read data from the CSV file
+
     data = pd.read_csv(csv_file)
 
     # Extract the columns you want to plot
@@ -38,7 +38,7 @@ def draw_boxplot_from_csv(csv_file, _name):
 
     plt.boxplot(transposed_data.values, labels=data['Name'], notch=False, sym='+', vert=True, whis=1.5)
 
-    plt.xlabel("Tools' names (nbr of operations)")
+    plt.xlabel("Tools' names")
     plt.ylabel("Error")
     plt.title("{} error".format(_name))
 
@@ -53,12 +53,23 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Visualize pose estimation accuracy')
     parser.add_argument('--pos_diff_csv', type=str, help='The path to the csv file with position difference',
-                            required=True)
+                            required=False)
     parser.add_argument('--rot_diff_csv', type=str, help='The path to the csv file with rotation difference',
-                            required=True)
-    parser.add_argument('--out_path', type=str, help='The save path for plots', required=True)
+                            required=False)
+    parser.add_argument('--out_path', type=str, help='The save path for plots', required=False)
     args = parser.parse_args()
-    plot_rotation_accuracy(args.rot_diff_csv, args.out_path)
-    plot_position_accuracy(args.pos_diff_csv, args.out_path)
-    draw_latex_table(args.pos_diff_csv)
-    draw_latex_table(args.rot_diff_csv)
+    save_path = create_out_directory(args.out_path)
+    #TODO: add the logging
+    print("Save directory: ", save_path)
+    if args.rot_diff_csv:
+        print("Plotting rotation difference")
+        plot_rotation_accuracy(args.rot_diff_csv, save_path)
+        draw_latex_table(args.rot_diff_csv)
+    else:
+        print("No rotation difference csv file provided")
+    if args.pos_diff_csv:
+        print("Plotting position difference")
+        plot_position_accuracy(args.pos_diff_csv, save_path)
+        draw_latex_table(args.pos_diff_csv)
+    else:
+        print("No position difference csv file provided")
