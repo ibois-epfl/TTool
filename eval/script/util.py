cd769929d0fab26168a4f4176d45c1340aec31ef
sushidelivery <nazgul160796@icloud.com>
2023-11-09 17:30:20 +0100

    ADD: create subfolders in out directory

--- eval/script/util.py
+++ eval/script/util.py
@@ -3,11 +3,16 @@ import os
 
 def create_out_dir(out_path):
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    default_path = os.path.abspath(os.path.join(script_dir, "..", ".."))
-    if out_path is not None:
-        out_dir = os.path.join(out_path, 'out')
-    else:
-        out_dir = os.path.join(default_path, 'out')
-    os.makedirs(out_dir, exist_ok=True)
-    return out_dir
+    default_path = os.path.abspath(os.path.join(script_dir, ".."))  # Go up one level from the script directory
+    base_out_dir = os.path.join(out_path if out_path else default_path, 'out')
+
+    # Create subdirectories for 'csv', 'latex', and 'boxplot'
+    subdirs = ['csv', 'latex', 'boxplot']
+    paths = {}
+    for subdir in subdirs:
+        dir_path = os.path.join(base_out_dir, subdir)
+        os.makedirs(dir_path, exist_ok=True)
+        paths[subdir] = dir_path
+
+    return paths
 
